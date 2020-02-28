9d4af5a1e8a63adfc657e09febc6d56571bc0079
PAREEK <rahul.pareek@dxc.com>
2020-02-28 09:50:57 +0530

    GreyStream Folder Scripts Structure

---
+++ Scripts/Main.js
@@ -0,0 +1,26 @@
+﻿var lng = $("#langselector").val;
+var cnt = $("#contents").val;
+var reg = $("#register").val;
+
+$(document).ready(function () {
+
+    loadlang();
+});
+
+function loadlang() {
+  
+    switch (lng) {
+        case "hn":
+            cnt.src = "Hindi.html";
+            document.title = "ग्रैस्ट्रीम | घर";
+            reg.innerHTML = "पंजीकरण";
+            break;
+        case "en":
+            cnt.src = "Eng.html";
+            document.title = "Greystream | Home";
+            reg.innerHTML = "Registration";
+            break;
+        default:
+            cnt.src = "Hindi.html";
+    }
+}
